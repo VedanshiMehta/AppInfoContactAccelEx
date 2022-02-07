413f9692650b1c7b3ede5c9d97e6485d077abbc3
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-02-07 19:07:24 +0530

    Add project files.

---
+++ AppInfoContactAccelEx/ContactEx.cs
@@ -0,0 +1,84 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace AppInfoContactAccelEx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    class ContactEx : Activity
+    {
+        private Button mButton;
+        private TextView mtext1;
+        private TextView mtext2;
+        private TextView mtext3;
+        private TextView mtext4;
+        private TextView mtext5;
+        private TextView mtext6;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ContactInfoDemo);
+            UIReference();
+            UIClickEvent();
+        }
+
+        private void UIClickEvent()
+        {
+            GetContactInfoAsync();
+        }
+
+        private async Task GetContactInfoAsync()
+        {
+            try
+            {
+              
+                var contact = await Contacts.PickContactAsync();
+               ;
+
+
+                if (contact == null)
+               
+
+                return;
+
+                
+                mtext1.Text = contact.Id;
+                mtext2.Text = contact.NamePrefix;
+                mtext3.Text = contact.GivenName;
+                mtext4.Text = contact.MiddleName;
+                mtext5.Text = contact.Phones.ToString();
+                mtext6.Text = contact.Emails.ToString();
+              
+
+
+            }
+            catch (Exception e)
+            {
+                Log.Debug("Execption", e.StackTrace);
+            
+            }
+            
+        }
+
+        private void UIReference()
+        {
+            mButton = FindViewById<Button>(Resource.Id.buttonCI);
+            mtext1 = FindViewById<TextView>(Resource.Id.textViewC1);
+            mtext2 = FindViewById<TextView>(Resource.Id.textViewC2);
+            mtext3 = FindViewById<TextView>(Resource.Id.textViewC3);
+            mtext4 = FindViewById<TextView>(Resource.Id.textViewC4);
+           mtext5 = FindViewById<TextView>(Resource.Id.textViewC5);
+            mtext6 = FindViewById<TextView>(Resource.Id.textViewC6);
+        }
+    }
+}
---
+++ AppInfoContactAccelEx/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace AppInfoContactAccelEx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button myAppInfo;
+        Button myContact;
+        Button myAccelometer;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+            myAppInfo = FindViewById<Button>(Resource.Id.button1);
+            myContact = FindViewById<Button>(Resource.Id.button2);
+            myAccelometer = FindViewById<Button>(Resource.Id.button3);
+
+
+            myAppInfo.Click += MyAppInfo_Click;
+            myContact.Click += MyContact_Click;
+            myAccelometer.Click += MyAccelometer_Click;
+        }
+
+        private void MyAccelometer_Click(object sender, System.EventArgs e)
+        {
+            Intent k = new Intent(Application.Context, typeof(AccelrometerEx));
+            StartActivity(k);
+        }
+
+        private void MyContact_Click(object sender, System.EventArgs e)
+        {
+
+            Intent j = new Intent(Application.Context, typeof(ContactEx));
+            StartActivity(j);
+        }
+
+        private void MyAppInfo_Click(object sender, System.EventArgs e)
+        {
+            Intent i = new Intent(Application.Context, typeof(AppInfoEx));
+            StartActivity(i);
+        }
+
+        private void MyPropertyDemo_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+
+
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
